a3d62f63a439ba82cfed0078e8704ed83dc67a7b
Feruz <Feruzshoh.77@gmail.com>
2023-03-05 10:34:59 +0300

    yangi holat

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ 
+ <h3>Yangi qidiruv tizim</h3>  <h1>SAFAR</h1> 
+
+
+    </div>
+  );
+}
+
+export default App;
