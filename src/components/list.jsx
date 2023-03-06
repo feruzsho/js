e189f146c0c7852297c93a7e9eb39a31a0f5094f
Feruz <Feruzshoh.77@gmail.com>
2023-03-06 11:41:50 +0500

    list va adata

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,6 +1,40 @@
-import {useState, Component} from 'react';
-import List from './components/List';
-import { data } from './data';
-import { users } from './data';
-import './App.css';
 
+import './List.css';
+
+const List =(props)=>{
+    const { setPeople} = props;
+    console.log(props);
+    function clearAll()
+    {
+        setPeople([]);
+    }
+    return (
+        <div className="preson">
+            <ul className="container">
+                { props.arr.map((itemObj)=>{
+                  return (
+                      <li key={itemObj.id}>
+                          <div>
+                              <img 
+                              className="image"
+                                  src={itemObj.image}
+                                  alt="no image"
+                                  width={100}
+                                  height={100}
+                                  />
+                              </div>
+                              <div>
+                  <h3>{itemObj.name}</h3>
+                  <p>{itemObj.age}</p>
+                              </div>
+                      </li>
+                  );
+
+                })}
+            </ul>
+            <button onClick={clearAll}>Clear All</button>
+        </div>
+    );
+};
+
+export default List;
---
+++ src/data.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+      id: 1,
+      name: 'Susan',
+      age: 33,
+      image: 'https://image.cnbcfm.com/api/v1/image/101532264-111215.074-copy?v=1395947028',
+    },
+    {
+      id: 2,
+      name: 'Tommy',
+      age: 28,
+      image: 'https://www.tradeinn.com/f/13794/137946630/tommy-hilfiger-core-logo-hoodie.jpg',
+    },
+    {
+      id: 3,
+      name: 'John',
+      age: 20,
+      image:
+        'https://images.squarespace-cdn.com/content/v1/55db3b44e4b01e5be14cad77/1657893813325-56Q5M5A6GELA24XFRKUU/Screenshot+2022-07-15+at+16.03.18.png?format=1500w',
+    },
+    {
+      id: 4,
+      name: 'Lola',
+      age: 34,
+      image:
+        'https://media.glamour.com/photos/62a005a99cf458ef2ec69272/master/pass/SITP_S1_UT_101_210726_HAWDAN_00879RC.jpg',
+    },
+    {
+      id: 5,
+      name: 'hezal',
+      age: 18,
+      image:
+        'https://textildealcdn.sgp1.digitaloceanspaces.com/uploads/1676440009-Aanchi-Kidress-Little-Hezal-Wholesale-Kids-Kurtis-5.jpg',
+    },
+  ];
+  
